6b6cd7ad9c0e5d00a1d1be2459f4361783e98434
muthoni <muthoni.ndehi@student.moringaschool.com>
2022-09-08 12:52:39 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 function isPalindrome(word) {
-  // Write your algorithm here
-}
+  for (let i = 0; i < word.length / 2; i++){
+    const f = word.length - 1 - i;
+    if(word[i] !==word[f]) {
+      return false
+    }
+  }
+  return true;
+  }
+  
+  // You can run `node index.js` to view these console logs
+  if (require.main === module) {
+    // add your own custom tests in here
+    console.log("Expecting: true");
+    console.log("=>", isPalindrome("racecar"));
+  
+    console.log("");
+  
+    console.log("Expecting: false");
+    console.log("=>", isPalindrome("robot"));
+  }
+  
+  module.exports = isPalindrome;
 
-/* 
-  Add your pseudocode here
-*/
-
-/*
-  Add written explanation of your solution here
-*/
-
-// You can run `node index.js` to view these console logs
-if (require.main === module) {
-  // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", isPalindrome("racecar"));
-
-  console.log("");
-
-  console.log("Expecting: false");
-  console.log("=>", isPalindrome("robot"));
-}
-
-module.exports = isPalindrome;
+  
